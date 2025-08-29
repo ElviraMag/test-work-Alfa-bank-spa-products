f42430369c70a9dc6d725c128ec892fc24c6601c
ElviraMag <amyleen008@gmail.com>
2025-08-29 18:29:51 +0300

    fix: обновила проект

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { ProductsPage } from './pages/ProductsPage';
 import { CreateProduct } from './pages/CreateProduct';
 import ProductDetail from './pages/ProductDetail';
